8a7663ac49ab6b4c556b7dbe4d29c318c45321c1
Mozahid RANA <mozahidpic@gmail.com>
2023-02-08 02:29:05 +1100

    live backend url changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,7 +12,8 @@ export const Home = () => {
     const fetchPosts = async () => {
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        // local link: http://localhost:8080/api/v1/post
+        const response = await fetch('https://dalle-clone-9m94.onrender.com/api/v1/post', {
           method: 'GET',
           headers: { 'Content-Type': 'application/json' },
         });
